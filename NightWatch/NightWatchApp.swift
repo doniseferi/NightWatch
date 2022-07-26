56e3b59b599cf5d17a9812b821c90e747b3523c7
Edonis Seferi <edonisseferi@tfl.gov.uk>
2022-07-26 10:38:47 +0100

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Edonis Seferi on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
