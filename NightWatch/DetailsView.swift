dbe8e37bce497daf6b321b4a7a36a8d46816c9fa
Edonis Seferi <edonisseferi@tfl.gov.uk>
2022-08-14 12:00:50 +0100

    show view based on size

--- NightWatch/DetailsView.swift
+++ NightWatch/DetailsView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 struct DetailsView: View {
     
     @Binding var task: Task
-    
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+
     var body: some View {
         VStack {
             Text(task.name )
+            
             Text("Placeholder details 1")
-            Text("Placeholder details 2")
+            
+            Divider()
+            
+            if verticalSizeClass == .regular {
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .padding([.leading, .trailing], 10)
+            
+                Divider()
+            }
             
             Button(task.isComplete
                    ? "Reactivate"
@@ -28,6 +38,17 @@ struct DetailsView: View {
 
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailsView(task: Binding<Task>.constant(Task(name: "Close all windows", isComplete: false, lastCompleted: nil)))
+        Group {
+            
+        DetailsView(task: Binding<Task>.constant(Task(name: "Close all doors", isComplete: false, lastCompleted: nil)))
+            .previewInterfaceOrientation(.portrait)
+        
+        
+            DetailsView(task: Binding<Task>.constant(Task(name: "Close all doors", isComplete: false, lastCompleted: nil)))
+                .previewInterfaceOrientation(.portrait)
+                .previewLayout(.fixed(width: 667, height: 375))
+            
+            
+        }
     }
 }
